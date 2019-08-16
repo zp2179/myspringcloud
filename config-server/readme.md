25ac9494521829faa64b43b63cda016e58d87045
zhupeng <zp2179@163.com>
2019-08-16 09:42:11 +0800

    init

---
+++ config-server/readme.md
@@ -0,0 +1,8 @@
+
+- http请求地址和资源文件映射如下:
+
+    - /{application}/{profile}[/{label}]
+    - /{application}-{profile}.yml
+    - /{label}/{application}-{profile}.yml
+    - /{application}-{profile}.properties
+    - /{label}/{application}-{profile}.properties
---
+++ service-feign/src/main/java/com/joker/springcloud/servicefeign/api/SchedualServiceHiHystric.java
@@ -0,0 +1,13 @@
+package com.joker.springcloud.servicefeign.api;
+
+import com.joker.springcloud.servicefeign.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+
+}
